00788ec3b4aba553f9a29e12028c0bb8468d3996
Ernestocmd <emedina@utem.cl>
2024-08-10 19:40:27 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // Esto hace que Vite sea accesible en la red local
+    port: 3000,  // Cambia el puerto si es necesario
+  }
 })
