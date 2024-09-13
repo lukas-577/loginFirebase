1647500fd5ba0528f0664bcfcf6d7869c0415f68
lukas <lukas_skyry_14@hotmail.cl>
2024-09-12 23:34:06 -0300

    agrego boton ubicacion y agregar

--- src/pages/Maps/components/ContenedorMaps.jsx
+++ src/pages/Maps/components/ContenedorMaps.jsx
@@ -3,12 +3,13 @@ import { MapContainer, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import Markers from './Markers';
 
-function ContenedorMaps({ position }) {
+function ContenedorMaps({ position, mapRef }) {
 
     console.log(position)
+    console.log(mapRef)
 
     return (
-        <MapContainer center={position} zoom={13} style={{ height: '100vh', width: '100%' }}>
+        <MapContainer center={position} zoom={13} style={{ height: '100vh', width: '100%', zIndex: '0' }} ref={mapRef}>
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png">
 
             </TileLayer>
