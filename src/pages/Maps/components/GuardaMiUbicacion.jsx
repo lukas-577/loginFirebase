d345c7d5ffbe3162c241ffd6626c145b873d3856
Enzogus <etroncoso@utem.cl>
2024-10-29 01:56:51 -0300

    feature guardar ubicacion firestore

--- src/pages/Maps/components/GuardaMiUbicacion.jsx
+++ src/pages/Maps/components/GuardaMiUbicacion.jsx
@@ -1,31 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import 'boxicons';
+import { getFirestore, collection, doc, setDoc } from 'firebase/firestore';
+import { useAuth } from '../../../context/AuthContext';
 
 const GuardaMiUbicacion = ({ position }) => {
-    const addLocation = () => {
+    const { user } = useAuth(); // Obtiene el usuario autenticado desde el contexto
+    const db = getFirestore(); // Inicializa Firestore
 
+    const addLocation = async () => {
         if (!position) return;
 
+        if (!user) {
+            console.error("No hay un usuario autenticado.");
+            return;
+        }
+
+        // Crea el objeto de ubicación
         const location = {
             lat: position.lat,
             lng: position.lng,
             date: new Date().toLocaleString()
+        };
+
+        // Genera un ID basado en las coordenadas
+        const locationId = `${position.lat}-${position.lng}`; // ID personalizado
+
+        try {
+            // Obtén la referencia de la subcolección 'locations' y define el ID del documento
+            const userDocRef = doc(db, 'users', user.uid); // Referencia al documento del usuario
+            const locationDocRef = doc(userDocRef, 'locations', locationId); // Referencia al documento dentro de la subcolección 'locations' con ID personalizado
+
+            // Guarda la ubicación en Firestore con el ID personalizado
+            await setDoc(locationDocRef, location);
+            console.log("Ubicación guardada exitosamente:", location);
+        } catch (error) {
+            console.error("Error al guardar la ubicación:", error);
         }
-        console.log(location)
-        const locations = JSON.parse(localStorage.getItem('locations')) || []
-        locations.push(location)
-        localStorage.setItem('locations', JSON.stringify(locations))
-        console.log(locations)
-    }
+    };
 
     return (
         <div className="absolute bottom-36 right-4 z-50">
             <button className="btn btn-success" onClick={addLocation}>
-
                 <box-icon name='heart'></box-icon>Agregar
             </button>
         </div>
-    )
-}
+    );
+};
 
 export default GuardaMiUbicacion;
