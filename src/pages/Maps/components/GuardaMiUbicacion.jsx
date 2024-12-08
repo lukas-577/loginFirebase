d47918a17eeaf88e206ca98821fb434e60058d24
lukas <lukas_skyry_14@hotmail.cl>
2024-12-08 11:49:30 -0300

    uso modal daisyui

--- src/pages/Maps/components/GuardaMiUbicacion.jsx
+++ src/pages/Maps/components/GuardaMiUbicacion.jsx
@@ -2,12 +2,10 @@ import React, { useState } from 'react';
 import 'boxicons';
 import { getFirestore, collection, doc, setDoc } from 'firebase/firestore';
 import { useAuth } from '../../../context/AuthContext';
-import Modal from 'react-modal';
 
 const GuardaMiUbicacion = ({ position }) => {
     const { user } = useAuth();
     const db = getFirestore();
-    const [isModalOpen, setIsModalOpen] = useState(false);
     const [locationName, setLocationName] = useState('');
     const [showSnackbar, setShowSnackbar] = useState(false);
     const [snackbarVisible, setSnackbarVisible] = useState(false); // Estado para manejar la visibilidad gradual
@@ -47,40 +45,43 @@ const GuardaMiUbicacion = ({ position }) => {
         }
     };
 
-    const handleAddLocationAndClose = () => {
+    const handleAddLocation = () => {
         addLocation();
-        setIsModalOpen(false);
         setLocationName('');
     };
 
     return (
         <div className="absolute bottom-36 right-4 z-50">
-            <button className="btn btn-success" onClick={() => setIsModalOpen(true)}>
+            <button className="btn btn-success" onClick={() => document.getElementById('my_modal_1').showModal()}>
                 <box-icon name='heart'></box-icon>Agregar
             </button>
-            <Modal
-                isOpen={isModalOpen}
-                onRequestClose={() => setIsModalOpen(false)}
-                className="w-full max-w-sm p-6 bg-white rounded-lg shadow-lg"
-                overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
-            >
-                <h2 className="text-lg font-bold mb-4">Ingrese el nombre de la ubicación</h2>
-                <input
-                    type="text"
-                    value={locationName}
-                    onChange={(e) => setLocationName(e.target.value)}
-                    className="form-control mb-4 bg-gray-300 rounded-lg placeholder-gray-500 p-2 text-gray-800"
-                    placeholder="Nombre de la ubicación"
-                />
-                <button className="btn btn-primary" onClick={handleAddLocationAndClose}>Agregar</button>
-            </Modal>
+
+            <dialog id="my_modal_1" className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Agregar Ubicación</h3>
+                    <p className="py-4">Se agregara la ubicación a tu perfil</p>
+                    <input
+                        type="text"
+                        placeholder="Nombre de la ubicación"
+                        className="input input-bordered input-success w-full max-w-xs"
+                        value={locationName}
+                        onChange={(e) => setLocationName(e.target.value)}
+                    />
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn" onClick={handleAddLocation}>Agregar</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+
 
             {/* Snackbar con desvanecimiento */}
             {showSnackbar && (
                 <div
-                    className={`fixed bottom-4 right-4 bg-green-600 text-white p-3 rounded shadow-md transition-opacity duration-500 ${
-                        snackbarVisible ? 'opacity-100' : 'opacity-0'
-                    }`}
+                    className={`fixed bottom-4 right-4 bg-green-600 text-white p-3 rounded shadow-md transition-opacity duration-500 ${snackbarVisible ? 'opacity-100' : 'opacity-0'
+                        }`}
                 >
                     Ubicación guardada exitosamente
                 </div>
