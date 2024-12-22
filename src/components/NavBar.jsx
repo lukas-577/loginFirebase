595eb32909da413c3af36ce4bd76a26e123fa442
lukas <lukas_skyry_14@hotmail.cl>
2024-12-22 00:02:10 -0300

    agrego logo de Icap en la barra de navegación

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -23,6 +23,9 @@ export default function NavBar({ user }) {
     return (
         <div className="fixed top-0 w-full navbar bg-primary text-white shadow-md z-10">
             <div className="flex-1">
+                <div className="w-10 rounded-full">
+                    <img src="../../public/planta.svg" alt="Icap" />
+                </div>
                 <a className="btn btn-ghost text-xl" href='/'>Icap</a>
             </div>
             <div className="flex-none gap-4">
