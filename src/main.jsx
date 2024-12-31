e6cd8d59ed0e666a7ad5b3b8be93a19813139398
lukas <lukas_skyry_14@hotmail.cl>
2024-12-31 15:38:37 -0300

    optimizar flujo de trabajo en GitHub Actions y corregir nombre de archivo CSS

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import router from './routes/Router.jsx'
 import './index.css'
-import './app.css'
+import './App.css'
 import { RouterProvider } from "react-router-dom"
 import { AuthProvider } from './context/AuthContext.jsx'
 import { PhotoProvider } from './context/PhotoContext.jsx'
